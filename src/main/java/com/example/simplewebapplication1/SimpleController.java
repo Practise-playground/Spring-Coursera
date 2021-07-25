774070c8a37aa51e23a88cac3cf0f0b79f183b92
shalitha <shalithaanuradha123@gmail.com>
2021-07-25 15:18:59 +0530

    My first course followed spring project.

---
+++ src/main/java/com/example/simplewebapplication1/SimpleController.java
@@ -0,0 +1,20 @@
+package com.example.simplewebapplication1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Shalitha Anuradha <shalithaanuradha123@gmail.com>
+ * @since : 2021-07-25
+ **/
+@RestController
+public class SimpleController {
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World yeah";
+    }
+    @RequestMapping("/a")
+    public String indexa(){
+        return "Hello World A";
+    }
+}
